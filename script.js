d931dcc5b77e6051ff0abf46c9c9763ffb230571
Navneet_thakur <127828104+navneet451@users.noreply.github.com>
2024-10-22 16:55:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //your JS code here. If required.
+function removeSelectedColor() {
+            let colorSelect = document.getElementById("colorSelect");
+
+            // Remove the selected option
+            colorSelect.remove(colorSelect.selectedIndex);
+        }
